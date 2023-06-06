d630a2dcc8a17381ab62d9867f864d74efd54ab2
Viktor Malmedal <viktor@malmedal.dev>
2023-06-06 12:49:41 +0200

    feat: add root layout

--- apps/frag-fusion/src/app/layout.tsx
+++ apps/frag-fusion/src/app/layout.tsx
@@ -1,9 +1,53 @@
 import "@ff/ui/globals.css";
+import { Inter } from "next/font/google";
+import LocalFont from "next/font/local";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import { cn } from "@ff/ui";
+
+import { siteConfig } from "~/config";
+import { ThemeProvider } from "~/providers/theme-provider";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const calSans = LocalFont({
+  src: "../styles/calsans.ttf",
+  variable: "--font-cal",
+});
+
+export const metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    images: [{ url: "/opengraph-image.png" }],
+  },
+};
 
 export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            calSans.variable,
+            inter.variable,
+          )}
+        >
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
