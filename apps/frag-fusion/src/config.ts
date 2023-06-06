f705d63a2e63f33f389c9d3ec34838bf066a50a4
Viktor Malmedal <viktor@malmedal.dev>
2023-06-06 12:50:08 +0200

    feat: add site config

---
+++ apps/frag-fusion/src/config.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "Frag Fusion",
+  description:
+    "Frag Fusion is a premier CS:GO gaming platform that brings together passionate gamers from around the world. Join our vibrant community and experience competitive matchmaking, organized tournaments, and skill-based rankings. Enhance your gameplay, connect with like-minded players, and showcase your skills in exhilarating CS:GO battles. Dive into the ultimate fusion of competition and camaraderie at Frag Fusion.as",
+};
