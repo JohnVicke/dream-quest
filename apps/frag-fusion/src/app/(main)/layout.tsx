096534e0e8c3e876b44081f242c25a64b0dcf8e4
Viktor Malmedal <viktor@malmedal.dev>
2023-06-06 20:11:00 +0200

    fix: ts-ignore...

--- apps/frag-fusion/src/app/(main)/layout.tsx
+++ apps/frag-fusion/src/app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import { TopNavigation } from "./top-nav";
 export default function MainLayout({ children }: React.PropsWithChildren) {
   return (
     <>
+      {/* @ts-ignore React server component */}
       <TopNavigation />
       <div className="grid h-screen grid-cols-[1fr,auto] overflow-hidden pt-16">
         <main className="mx-auto w-full max-w-screen-xl overflow-y-auto pt-16">
