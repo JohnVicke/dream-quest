f6b85092f23dfaa0531a947fff99f2f166609bd2
ViktorM <viktor@malmedal.dev>
2023-06-07 15:10:43 +0200

    type-check

--- packages/config/tailwind-config/index.ts
+++ packages/config/tailwind-config/index.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import animatePlugin from "tailwindcss-animate";
 
 export default {
   darkMode: ["class"],
@@ -68,5 +69,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animatePlugin],
 } satisfies Config;
