c438058974e2511b28777044c2b390896ab14f09
ViktorM <viktor@malmedal.dev>
2023-07-05 14:17:57 +0200

    feat: update site config

--- apps/dreamquest-engine/src/config.ts
+++ apps/dreamquest-engine/src/config.ts
@@ -1,5 +1,6 @@
 export const siteConfig = {
-  name: "Frag Fusion",
+  name: "DreamQuest",
   description:
-    "Frag Fusion is a premier CS:GO gaming platform that brings together passionate gamers from around the world. Join our vibrant community and experience competitive matchmaking, organized tournaments, and skill-based rankings. Enhance your gameplay, connect with like-minded players, and showcase your skills in exhilarating CS:GO battles. Dive into the ultimate fusion of competition and camaraderie at Frag Fusion.as",
+    "DreamQuest is a community platform for sharing and discussing ideas, " +
+    " and building communities.",
 };
