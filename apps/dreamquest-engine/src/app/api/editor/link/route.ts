5378b0317c5e9c6e6532370d2d7ac3ef6b9332bd
Viktor Malmedal <viktor@malmedal.dev>
2023-07-07 09:41:45 +0200

    format links to editorjs

---
+++ apps/dreamquest-engine/src/app/api/editor/link/route.ts
@@ -0,0 +1,35 @@
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const href = url.searchParams.get("url");
+
+  if (!href) {
+    return new Response("Invalid href", { status: 400 });
+  }
+
+  const res = await fetch(href);
+  const data = await res.text();
+
+  const titleMatch = data.match(/<title>(.*?)<\/title>/);
+  const title = titleMatch ? titleMatch[1] : "";
+
+  const descriptionMatch = data.match(
+    /<meta name="description" content="(.*?)"/,
+  );
+  const description = descriptionMatch ? descriptionMatch[1] : "";
+
+  const imageMatch = data.match(/<meta property="og:image" content="(.*?)"/);
+  const imageUrl = imageMatch ? imageMatch[1] : "";
+
+  return new Response(
+    JSON.stringify({
+      success: 1,
+      meta: {
+        title,
+        description,
+        image: {
+          url: imageUrl,
+        },
+      },
+    }),
+  );
+}
