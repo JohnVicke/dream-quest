2508105c0e1a0477d65ddd1ac52fb8451b4f81e5
Viktor Malmedal <viktor@malmedal.dev>
2023-07-07 09:42:10 +0200

    populate combobox

--- apps/dreamquest-engine/src/modules/layout/_top-nav/index.tsx
+++ apps/dreamquest-engine/src/modules/layout/_top-nav/index.tsx
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic";
 import Link from "next/link";
 
+import { db } from "@dq/db";
+
 import { AuthNavContent } from "./auth-nav-content";
 import { CommunityCombobox } from "./community-combobox";
 
@@ -8,7 +10,9 @@ const ThemeToggleSelect = dynamic(() => import("./theme-toggle-select"), {
   ssr: false,
 });
 
-export function TopNavigation() {
+export async function TopNavigation() {
+  const communities = await db.query.community.findMany();
+
   return (
     <div className="fixed top-0 z-10 flex h-16 w-full items-center justify-between space-x-4 border-b bg-background px-8">
       <nav className="flex items-center space-x-4">
@@ -18,7 +22,7 @@ export function TopNavigation() {
         >
           Dreamquest
         </Link>
-        <CommunityCombobox />
+        <CommunityCombobox communities={communities} />
       </nav>
       <div className="flex gap-x-2">
         <ThemeToggleSelect />
