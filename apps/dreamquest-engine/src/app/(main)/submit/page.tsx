18d61fa36319c7cd631ab84470e37a97acdae950
Viktor Malmedal <viktor@malmedal.dev>
2023-07-07 16:36:12 +0200

    editor with select

--- apps/dreamquest-engine/src/app/(main)/submit/page.tsx
+++ apps/dreamquest-engine/src/app/(main)/submit/page.tsx
@@ -1,11 +1,23 @@
 import { auth } from "@clerk/nextjs";
 
-import { CreatePostForm } from "~/modules/posts/create-post-form";
+import { db, eq, schema } from "@dq/db";
 
-export default function SubmitPage() {
-  const userId = auth();
+import { Editor } from "~/modules/posts/editor/editor";
+
+export default async function SubmitPage() {
+  const { userId } = auth();
+
   if (!userId) {
     return null;
   }
-  return <CreatePostForm />;
+
+  const communities = await db.query.community.findMany({
+    with: {
+      subscriptions: {
+        where: eq(schema.subscription.userId, userId),
+      },
+    },
+  });
+
+  return <Editor communities={communities} />;
 }
