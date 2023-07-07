f890d77e50d26dc261b0796c2ddf0ff54ac4dc2a
Viktor Malmedal <viktor@malmedal.dev>
2023-07-07 16:34:11 +0200

    post with editor

--- apps/dreamquest-engine/src/app/(main)/c/[name]/[postId]/page.tsx
+++ apps/dreamquest-engine/src/app/(main)/c/[name]/[postId]/page.tsx
@@ -1,8 +1,13 @@
+import { cache } from "react";
+import { Metadata, ResolvingMetadata } from "next";
 import { notFound } from "next/navigation";
-import { Trash } from "lucide-react";
+import { auth } from "@clerk/nextjs";
 
 import { db, eq, schema } from "@dq/db";
-import { Button } from "@dq/ui/button";
+
+import { PostDisplay } from "~/modules/posts/post-display";
+import { RemovePostButton } from "~/modules/posts/remove-post-button";
+import { ReactQueryProvider } from "~/providers/react-query-provider";
 
 interface CommunityPostPageProps {
   params: {
@@ -10,12 +15,37 @@ interface CommunityPostPageProps {
   };
 }
 
+const selectPost = cache(async (id: number) => {
+  return db.query.post.findFirst({
+    where: eq(schema.post.id, id),
+    with: {
+      votes: true,
+      creator: true,
+    },
+  });
+});
+
+export async function generateMetadata(
+  { params }: CommunityPostPageProps,
+  _parent?: ResolvingMetadata,
+): Promise<Metadata> {
+  const id = params.postId;
+  const post = await selectPost(parseInt(id, 10));
+
+  if (!post) {
+    return notFound();
+  }
+
+  return {
+    title: post.title,
+  };
+}
+
 export default async function CommunityPostPage({
   params,
 }: CommunityPostPageProps) {
-  const post = await db.query.post.findFirst({
-    where: eq(schema.post.id, parseInt(params.postId, 10)),
-  });
+  const { userId } = auth();
+  const post = await selectPost(parseInt(params.postId, 10));
 
   if (!post) {
     return notFound();
@@ -23,11 +53,12 @@ export default async function CommunityPostPage({
 
   return (
     <div>
-      <h1>{post.title}</h1>
-      <p>{JSON.stringify(JSON.parse(post.content as string), null, 2)}</p>
-      <Button variant="destructive">
-        <Trash />
-      </Button>
+      <PostDisplay title={post.title} content={post.content as any} />
+      {userId === post.creatorId && (
+        <ReactQueryProvider>
+          <RemovePostButton id={post.id} />
+        </ReactQueryProvider>
+      )}
     </div>
   );
 }
