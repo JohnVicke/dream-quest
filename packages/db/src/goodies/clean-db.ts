838902237c7697823bffd86d0ea5ad5aae2dcca2
Viktor Malmedal <viktor@malmedal.dev>
2023-07-11 08:10:47 +0200

    delete friends

--- packages/db/src/goodies/clean-db.ts
+++ packages/db/src/goodies/clean-db.ts
@@ -1,12 +1,11 @@
 import { db } from "../db";
-import { community, friends, post, subscription, user, vote } from "../schema";
+import { community, post, subscription, user, vote } from "../schema";
 
 export async function clean() {
   const promises = [
     db.delete(post),
     db.delete(user),
     db.delete(vote),
-    db.delete(friends),
     db.delete(community),
     db.delete(subscription),
   ];
