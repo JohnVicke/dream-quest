be3722e60f4971d57f0acb03f1f38f83c33ec193
Viktor Malmedal <viktor@malmedal.dev>
2023-07-12 12:40:18 +0200

    share button with href or currentRoute

--- apps/dreamquest-engine/src/modules/posts/share-button.tsx
+++ apps/dreamquest-engine/src/modules/posts/share-button.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { Link, Share } from "lucide-react";
+import { Link } from "lucide-react";
 
-import { Button } from "@dq/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,11 +10,29 @@ import {
 } from "@dq/ui/dropdown-menu";
 import { useToast } from "@dq/ui/use-toast";
 
-export function ShareButton() {
+type ShareButtonProps =
+  | {
+      href: string;
+      currentRoute?: never;
+    }
+  | {
+      href?: never;
+      currentRoute: boolean;
+    };
+
+export function ShareButton({
+  children,
+  href,
+}: React.PropsWithChildren<ShareButtonProps>) {
+  if (!children) {
+    throw new Error("ShareButton must have a menu trigger as children");
+  }
   const { toast } = useToast();
 
   function copyToClipboard() {
-    navigator.clipboard.writeText(window.location.href);
+    navigator.clipboard.writeText(
+      `${window.location.origin}${href}` ?? window.location.href,
+    );
     toast({
       title: "Copied to clipboard",
       description: "The link has been copied to your clipboard.",
@@ -23,12 +40,7 @@ export function ShareButton() {
   }
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button>
-          <Share className="mr-4 h-4 w-4" />
-          Share
-        </Button>
-      </DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem className="cursor-pointer" onClick={copyToClipboard}>
           <Link className="mr-4 h-4 w-4" />
