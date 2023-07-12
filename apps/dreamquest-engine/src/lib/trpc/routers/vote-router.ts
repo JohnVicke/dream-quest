468d6e05806390853199b257aafa3b8de162141e
Viktor Malmedal <viktor@malmedal.dev>
2023-07-12 12:39:27 +0200

    use new tables

--- apps/dreamquest-engine/src/lib/trpc/routers/vote-router.ts
+++ apps/dreamquest-engine/src/lib/trpc/routers/vote-router.ts
@@ -1,3 +1,4 @@
+import { nanoid } from "nanoid";
 import { z } from "zod";
 
 import { and, db, eq, schema } from "@dq/db";
@@ -14,45 +15,55 @@ export const voteRouter = t.router({
       }),
     )
     .mutation(async ({ input, ctx }) => {
-      const vote = await db.query.vote.findFirst({
-        where: and(
-          eq(schema.vote.postId, input.postId),
-          eq(schema.vote.creatorId, ctx.user.id),
-        ),
-      });
+      const result = await db
+        .select()
+        .from(schema.vote)
+        .innerJoin(
+          schema.votesToPosts,
+          eq(schema.votesToPosts.voteId, schema.vote.id),
+        )
+        .where(
+          and(
+            eq(schema.vote.creatorId, ctx.user.id),
+            eq(schema.votesToPosts.postId, input.postId),
+          ),
+        );
+
+      const vote = result?.[0]?.vote;
 
       if (!vote) {
-        await db.insert(schema.vote).values({
-          creatorId: ctx.user.id,
-          postId: input.postId,
-          value: input.value,
-          updatedAt: new Timestamp(),
-          createdAt: new Timestamp(),
+        await db.transaction(async (trx) => {
+          const voteId = nanoid();
+          await trx.insert(schema.vote).values({
+            id: voteId,
+            creatorId: ctx.user.id,
+            value: input.value,
+            updatedAt: new Timestamp(),
+            createdAt: new Timestamp(),
+          });
+          await trx.insert(schema.votesToPosts).values({
+            postId: input.postId,
+            voteId,
+          });
         });
         return true;
       }
 
       if (input.value === vote.value) {
-        await db
-          .delete(schema.vote)
-          .where(
-            and(
-              eq(schema.vote.creatorId, ctx.user.id),
-              eq(schema.vote.postId, input.postId),
-            ),
-          );
+        console.log(vote.id);
+        await db.transaction(async (trx) => {
+          await trx.delete(schema.vote).where(eq(schema.vote.id, vote.id));
+          await trx
+            .delete(schema.votesToPosts)
+            .where(eq(schema.votesToPosts.voteId, vote.id));
+        });
         return true;
       }
 
       await db
         .update(schema.vote)
         .set({ value: input.value })
-        .where(
-          and(
-            eq(schema.vote.postId, input.postId),
-            eq(schema.vote.creatorId, ctx.user.id),
-          ),
-        );
+        .where(eq(schema.vote.id, vote.id));
 
       return true;
     }),
