7f607d94f68e4b8b3e2c5f526bdb09d610712995
Viktor Malmedal <viktor@malmedal.dev>
2023-07-12 09:04:25 +0200

    add new comment :)

--- apps/dreamquest-engine/src/modules/posts/comments/add-comment-form.tsx
+++ apps/dreamquest-engine/src/modules/posts/comments/add-comment-form.tsx
@@ -1,21 +1,61 @@
 "use client";
 
+import React from "react";
+import { useRouter } from "next/navigation";
+import { JSONContent } from "@tiptap/react";
 import { Plus } from "lucide-react";
 
 import { Button } from "@dq/ui/button";
+import { useToast } from "@dq/ui/use-toast";
 
 import { TipTapEditor } from "~/lib/tip-tap/editor";
+import { EditorContent } from "~/lib/tip-tap/editor-content";
+import { EditorContentLoading } from "~/lib/tip-tap/editor-content-loading";
 import { TipTapMenuBar } from "~/lib/tip-tap/editor-menu-bar";
+import { editorActions } from "~/lib/tip-tap/editor-menu-bar-actions";
+import { trpc } from "~/lib/trpc/client";
 
-export function AddCommentForm() {
+interface AddCommentFormProps {
+  postId: string;
+}
+
+export function AddCommentForm({ postId }: AddCommentFormProps) {
+  const { toast } = useToast();
+  const router = useRouter();
+  const mutation = trpc.comment.createPostComment.useMutation({
+    onSuccess: () => {
+      toast({
+        title: "Comment added",
+        description: "Your comment has been added to the post.",
+      });
+      router.refresh();
+    },
+  });
+
+  async function handleSubmit(content?: JSONContent) {
+    if (!content) {
+      toast({
+        title: "Comment is empty",
+        description: "Please add some content to your comment.",
+        variant: "destructive",
+      });
+      return;
+    }
+    await mutation.mutateAsync({ content, postId });
+  }
+
   return (
     <TipTapEditor>
-      <TipTapMenuBar>
-        <Button size="xs">
-          <Plus className="h-4 w-4" />
-          Add comment
-        </Button>
-      </TipTapMenuBar>
+      {({ getJSON }) => (
+        <EditorContent loadingContent={<EditorContentLoading />}>
+          <TipTapMenuBar actions={editorActions}>
+            <Button size="xs" onClick={() => handleSubmit(getJSON())}>
+              <Plus className="h-4 w-4" />
+              Add comment
+            </Button>
+          </TipTapMenuBar>
+        </EditorContent>
+      )}
     </TipTapEditor>
   );
 }
