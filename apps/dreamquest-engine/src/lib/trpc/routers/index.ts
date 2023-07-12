996b9ec95f813fd46ca6a2b5a98af39651d75188
Viktor Malmedal <viktor@malmedal.dev>
2023-07-12 09:06:17 +0200

    comment router

--- apps/dreamquest-engine/src/lib/trpc/routers/index.ts
+++ apps/dreamquest-engine/src/lib/trpc/routers/index.ts
@@ -1,4 +1,5 @@
 import { t } from "../trpc";
+import { commentRouter } from "./comment-router";
 import { communityRouter } from "./community-router";
 import { postRouter } from "./post-router";
 import { subscriptionRouter } from "./subscription-router";
@@ -11,6 +12,7 @@ export const router = t.router({
   vote: voteRouter,
   subscription: subscriptionRouter,
   user: userRouter,
+  comment: commentRouter,
 });
 
 export type Router = typeof router;
