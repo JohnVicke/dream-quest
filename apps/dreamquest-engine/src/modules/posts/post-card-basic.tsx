d71746b25fd8243be76ee9aeaca93d47e3fbd10e
Viktor Malmedal <viktor@malmedal.dev>
2023-07-12 12:40:01 +0200

    use voteserver

--- apps/dreamquest-engine/src/modules/posts/post-card-basic.tsx
+++ apps/dreamquest-engine/src/modules/posts/post-card-basic.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
-import { auth, clerkClient } from "@clerk/nextjs";
+import { MessageSquare, Share } from "lucide-react";
 
-import { and, db, eq, Post, schema, sql } from "@dq/db";
+import { db, eq, Post, schema, sql, User } from "@dq/db";
 
 import { getTimeSincePosted } from "~/utils/get-time-since-posted";
 import { HoverSubReddit } from "./hover-subredit";
-import { VoteControls } from "./vote-controls";
+import { ShareButton } from "./share-button";
+import { VoteServer } from "./votes/vote-server";
 
 interface PostCardBasicProps {
-  post: Post;
+  post: Post & { creator: User };
   communityAvatarUrl?: string | null;
   hideSubreddit?: boolean;
 }
@@ -18,31 +19,15 @@ export async function PostCardBasic({
   post,
   communityAvatarUrl,
 }: PostCardBasicProps) {
-  const sessionUser = auth();
-  const postUser = await clerkClient.users.getUser(post.creatorId);
-  const votes = await db
-    .select({
-      votes: sql<number>`sum(case 
-                             when ${schema.vote.value} = 'up' then 1
-                             when ${schema.vote.value} = 'down' then -1
-                         else 0 end)`,
-    })
-    .from(schema.vote)
-    .where(eq(schema.vote.postId, post.id));
-
-  const sessionVote = sessionUser.userId
-    ? await db.query.vote.findFirst({
-        where: and(
-          eq(schema.vote.postId, post.id),
-          eq(schema.vote.creatorId, sessionUser.userId),
-        ),
-      })
-    : null;
+  const nrComments = await db
+    .select({ count: sql<number>`count(*)`.mapWith(Number) })
+    .from(schema.comment)
+    .where(eq(schema.comment.postId, post.id));
 
   return (
     <div
       key={post.id}
-      className="relative flex gap-x-4 rounded-lg border shadow-lg"
+      className="relative flex items-start rounded-lg border hover:shadow-md"
     >
       <Link
         className="absolute inset-0"
@@ -50,15 +35,9 @@ export async function PostCardBasic({
       >
         <span className="hidden">{post.title}</span>
       </Link>
-      <VoteControls
-        className="z-10"
-        initialVotes={votes[0].votes ?? 0}
-        initialVote={sessionVote}
-        isAuthed={!!sessionUser.userId}
-        postId={post.id}
-      />
-      <div className="flex flex-col p-4">
-        <div className="flex items-end gap-x-2 [&>a]:z-10">
+      <VoteServer postId={post.id} direction="column" />
+      <div className="flex flex-col p-4 [&>div>a]:z-10">
+        <div className="flex items-end gap-x-2 ">
           {!hideSubreddit && (
             <HoverSubReddit
               post={post}
@@ -67,10 +46,26 @@ export async function PostCardBasic({
           )}
           <p className="flex items-center text-xs">
             {!hideSubreddit && <span className="mr-1">•</span>}
-            posted by {postUser.firstName} {getTimeSincePosted(post.createdAt)}
+            posted by {post?.creator?.username}{" "}
+            {getTimeSincePosted(post.createdAt)}
           </p>
         </div>
         <h3 className="my-2 text-xl font-bold">{post.title}</h3>
+        <div className="flex gap-x-1">
+          <Link
+            href={`/c/${post.communityName}/${post.id}#comment-section`}
+            className="z-10 flex w-fit items-center gap-x-1 rounded-md p-2 text-xs hover:bg-muted"
+          >
+            <MessageSquare className="h-4 w-4" />
+            {nrComments[0].count ?? 0} comments
+          </Link>
+          <ShareButton href={`/c/${post.communityName}/${post.id}`}>
+            <button className="z-10 flex w-fit items-center gap-x-1 rounded-md p-2 text-xs hover:bg-muted">
+              <Share className="h-4 w-4" />
+              Share
+            </button>
+          </ShareButton>
+        </div>
       </div>
     </div>
   );
