b68f3491e47cfbbde071246c3f746c9e9549f179
Viktor Malmedal <viktor@malmedal.dev>
2023-07-13 11:36:54 +0200

    export types

--- packages/db/src/types.ts
+++ packages/db/src/types.ts
@@ -3,6 +3,8 @@ import { InferModel } from "drizzle-orm";
 import * as schema from "./schema";
 
 export type Community = InferModel<typeof schema.community>;
+export type Subscription = InferModel<typeof schema.subscription>;
+export type Comment = InferModel<typeof schema.comment>;
 export type Post = InferModel<typeof schema.post>;
 export type Vote = InferModel<typeof schema.vote>;
 export type User = InferModel<typeof schema.user>;
