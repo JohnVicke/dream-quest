3f5cf196a935301d92c09aacf5c4f5594375f16f
Viktor Malmedal <viktor@malmedal.dev>
2023-07-13 11:36:01 +0200

    better auth nav

--- apps/dreamquest-engine/src/modules/layout/top-nav/auth-nav-content.tsx
+++ apps/dreamquest-engine/src/modules/layout/top-nav/auth-nav-content.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
-import { currentUser } from "@clerk/nextjs";
-import { LogOut, User } from "lucide-react";
+import { auth } from "@clerk/nextjs";
+import { LogOut, Settings } from "lucide-react";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@dq/ui/avatar";
+import { db, eq, schema } from "@dq/db";
 import { Button } from "@dq/ui/button";
 import {
   DropdownMenu,
@@ -11,12 +11,12 @@ import {
   DropdownMenuTrigger,
 } from "@dq/ui/dropdown-menu";
 
-import { initialsFromUser } from "~/utils/initials-from-user";
+import { UserAvatar } from "~/components/user-avatar";
 
 export async function AuthNavContent() {
-  const user = await currentUser();
+  const { userId } = auth();
 
-  if (!user) {
+  if (!userId) {
     return (
       <Link href="/signin" passHref>
         <Button>Sign in</Button>
@@ -24,7 +24,13 @@ export async function AuthNavContent() {
     );
   }
 
-  const initials = initialsFromUser(user);
+  const user = await db.query.user.findFirst({
+    where: eq(schema.user.id, userId),
+  });
+
+  if (!user) {
+    return null;
+  }
 
   return (
     <div className="flex gap-x-2">
@@ -33,12 +39,15 @@ export async function AuthNavContent() {
       </Link>
       <DropdownMenu>
         <DropdownMenuTrigger>
-          <Avatar>
-            <AvatarImage src={user.profileImageUrl} />
-            <AvatarFallback>{initials ? initials : <User />}</AvatarFallback>
-          </Avatar>
+          <UserAvatar src={user.profileImageUrl} />
         </DropdownMenuTrigger>
         <DropdownMenuContent>
+          <DropdownMenuItem>
+            <Link href="/settings" className="flex items-center gap-x-2">
+              <Settings className="h-4 w-4" />
+              Settings
+            </Link>
+          </DropdownMenuItem>
           <DropdownMenuItem>
             <Link href="/signout" className="flex items-center gap-x-2">
               <LogOut className="h-4 w-4" />
