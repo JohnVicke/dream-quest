e76b6fb86454fae6fc31c2ea0c0015eb2225dfc3
Viktor Malmedal <viktor@malmedal.dev>
2023-07-13 11:35:34 +0200

    layout fix

--- apps/dreamquest-engine/src/app/(moderator)/layout.tsx
+++ apps/dreamquest-engine/src/app/(moderator)/layout.tsx
@@ -1,12 +1,12 @@
+import { CommunitySettingsAside } from "~/modules/layout/community-settings-aside";
 import { LayoutWithLeftAside } from "~/modules/layout/layout-with-left-aside";
 import MainLayout from "~/modules/layout/main-layout";
-import { SettingsAside } from "~/modules/layout/settings-aside";
 
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
     <MainLayout>
-      <LayoutWithLeftAside asideContent={<SettingsAside />}>
-        <div className="mx-auto max-w-screen-lg pt-16">{children}</div>
+      <LayoutWithLeftAside asideContent={<CommunitySettingsAside />}>
+        {children}
       </LayoutWithLeftAside>
     </MainLayout>
   );
